19a526e1adb4aa9ac73e22ea85b76c5787fcfb17
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-07-05 18:21:33 +0300

    add logic into Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Post = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then(setPosts)
+      .catch(console.log);
+  }, []);
   return (
     <div>
       <h1>Posts</h1>
