ca40694970abc29b8efd6712a7af3f375a0ea88f
imnotoriginal <imnotoriginal@users.noreply.github.com>
2020-07-06 12:55:06 +0300

    add styles for Post component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
 import PostForm from "./components/PostForm/PostForm";
-import Posts from "./components/Posts";
+import Posts from "./components/Posts/Posts";
 
 const App = () => (
   <Provider store={store}>
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { fetchPosts } from "../store/actions";
+import { fetchPosts } from "../../store/actions";
+import styles from "./Posts.module.css";
 
 const Posts = ({ posts, fetchPosts }) => {
   useEffect(() => {
@@ -9,11 +10,11 @@ const Posts = ({ posts, fetchPosts }) => {
   }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Posts</h1>
       {posts.map((post) => (
-        <div key={post.id}>
-          <h5>{post.title}</h5>
+        <div className={styles.post} key={post.id}>
+          <h4>{post.title}</h4>
           <p>{post.body}</p>
         </div>
       ))}
